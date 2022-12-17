37a2991d83bd6417595831fa2c54c3b4bdfaa680
choimory <choimory@naver.com>
2022-12-17 13:11:33 +0900

    system: dotenv 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  //port
+  await app.listen(process.env.PORT);
+
+  //validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  //boot log
+  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+  console.log(`PORT: ${process.env.PORT}`);
 }
 bootstrap();
