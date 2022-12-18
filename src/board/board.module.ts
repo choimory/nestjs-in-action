b054bfb6661db7d914212fabcb03b6ecb1e3154c
choimory <choimory@naver.com>
2022-12-19 00:06:57 +0900

    fix: add board-comment to board module

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './controller/board.controller';
 import { BoardService } from './service/board.service';
+import { BoardCommentController } from './controller/board-comment.controller';
+import { BoardCommentService } from './service/board-comment.service';
 
 @Module({
-  controllers: [BoardController],
-  providers: [BoardService],
+  controllers: [BoardController, BoardCommentController],
+  providers: [BoardService, BoardCommentService],
 })
 export class BoardModule {}
